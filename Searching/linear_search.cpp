953f5debfefb747fb890cdebfe65489ead5ef6b3
Devanshu07R <devanshudasgupta142004@gmail.com>
2025-04-15 05:18:55 +0530

    Updated linear_search.cpp with improved logic and user input

--- Searching/linear_search.cpp
+++ Searching/linear_search.cpp
@@ -1,1 +1,28 @@
 //In the linear search algorithm, we search an element in a given array by Traversing the array from the starting till the ending till the entire element is not found..
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int LinearSearch(vector<int>arr,int tar){
+    for(int i = 0; i<arr.size();i++){
+        if(arr[i]==tar){
+            return i;
+        }//
+    }//
+
+    return -1;
+
+}
+int main(){
+    vector<int> arr = {1,34,67,45,89,32,0,-3};//index: 0,1,2,3,4,5,6,7
+    int target = 0;
+    int result = LinearSearch(arr,target);
+
+    if(result!=-1){
+        cout<<"element found at index:"<<result<<endl;
+    }else{
+        cout<<"Element not found."<<endl;
+    }
+    return 0;
+}
